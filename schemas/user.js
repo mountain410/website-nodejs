33e16acedc2a22610c1f64f07e74bbeac871c71d
mountain410 <502597986@qq.com>
2017-07-27 23:39:16 +0800

    'user-fix'

--- schemas/user.js
+++ schemas/user.js
@@ -1,6 +1,8 @@
 //mongoose 编译模式 定义数据字段的类型
 var mongoose = require('mongoose');
 
+var bcrypt = require('bcrypt')
+var SALT_WORK_FACTOR = 10
 
 // 定义数据字段的类型
 var UserSchema  = new mongoose.Schema({
@@ -10,6 +12,15 @@ var UserSchema  = new mongoose.Schema({
   },
   password: String,
   // 录入数据、更新数据的时间
+   // 0: nomal user
+  // 1: verified user
+  // 2: professonal user
+  // >10: admin
+  // >50: super admin
+  role: {
+    type: Number,
+    default: 0
+  },
   meta:{
     //录入数据时间
     createAt:{
@@ -32,8 +43,28 @@ UserSchema.pre('save',function(next){
   }else {
     this.meta.updateAt = Date.now();
   }
-  next(); //让流程继续走下去
+  bcrypt.genSalt(SALT_WORK_FACTOR, function(err, salt) {
+    if (err) return next(err)
+
+    bcrypt.hash(user.password, salt, function(err, hash) {
+      if (err) return next(err)
+
+      user.password = hash
+      next()
+    })
+  })
 })
+
+UserSchema.methods = {
+  comparePassword: function(_password, cb) {
+    bcrypt.compare(_password, this.password, function(err, isMatch) {
+      if (err) return cb(err)
+
+      cb(null, isMatch)
+    })
+  }
+}
+
 UserSchema.statics = {
   // 用来取出现在数据库所有的数据
   fetch:function(cb){
